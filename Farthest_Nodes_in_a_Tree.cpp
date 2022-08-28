8ef2788926dd679c3e6c6b1c3f954c0f786a2075
Md. Sajjat Hossen <111273800+Sajjat004@users.noreply.github.com>
2022-08-28 07:57:33 +0600

    Create Farthest_Nodes_in_a_Tree.cpp

---
+++ Farthest_Nodes_in_a_Tree.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long          ll;
+typedef long double        ld;
+typedef unsigned long long ull;
+typedef pair<int, int>     pii;
+typedef pair<ll, ll>       pll;
+typedef vector<int>        vi;
+typedef vector<ll>         vll;
+
+const int N      = (int) 2e5 + 5;
+const int mxN    = (int) 1e6 + 6;
+const int MOD    = (int) 1e9 + 7;
+const int INF    = (int) 1e9 + 9;
+const double EPS = (double) 1e-9;
+
+#define all(x)   x.begin(), x.end()
+#define szof(x)  (int) x.size()
+#define ff       first
+#define ss       second
+#define pb       push_back
+#define nl       '\n'
+#define PI       acos(-1.0)
+#define debug(x) cerr << #x << " = " << x << '\n';
+
+/*................. let's try one more time .................*/
+
+vector<pii> g[N];
+
+pii dfs(int u, int par, int d) {
+  pii ret = {d, u};
+  for (auto x : g[u]) {
+    int v = x.ff, w = x.ss;
+    if (v == par) continue;
+    pii temp = dfs(v, u, d + w);
+    if (temp > ret) ret = temp;
+  }
+  return ret;
+}
+
+int main() {
+#ifdef LOCAL
+  freopen("input.txt", "r", stdin);
+  // freopen("output.txt", "w", stdout);
+#endif
+  ios_base::sync_with_stdio(false); cin.tie(0);
+  int test = 1, tc = 0;
+  cin >> test;
+  while (test--) {
+    int n; cin >> n;
+    for (int i = 0; i <= n; ++i) {
+      g[i].clear();
+    }
+    for (int i = 1; i < n; ++i) {
+      int u, v, w; cin >> u >> v >> w;
+      g[u].pb({v, w});
+      g[v].pb({u, w});
+    }
+
+    pii res1 = dfs(0, -1, 0);
+    pii res2 = dfs(res1.ss, -1, 0);
+    cout << "Case " << ++tc << ": " << res2.ff << nl;
+  }
+  return 0;
+}
