dffbd0342006b913562221f9253a11b29f2c85fe
Md. Sajjat Hossen <111273800+Sajjat004@users.noreply.github.com>
2022-08-28 07:53:24 +0600

    Create Circle_in_Square.cpp

---
+++ Circle_in_Square.cpp
@@ -0,0 +1,73 @@
+/*-----------------------------------
+  bismillahir rahmanir rahim
+  @author : md. sajjat hossen
+  created : 
+------------------------------------*/
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long          ll;
+typedef long double        ld;
+typedef unsigned long long ull;
+typedef pair <int, int>    pii;
+typedef pair <ll, ll>      pll;
+typedef vector <int>       vi;
+typedef vector <ll>        vll;
+typedef tree <int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds_set;
+typedef tree <int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> pbds_multiset;
+
+inline int Int() { int x; scanf("%d", &x); return x; }
+inline ll Long() { ll x; scanf("%lld", &x); return x; }
+
+int dx[8] = { 0, -1, 0, 1, -1, -1, 1, 1 };
+int dy[8] = { -1, 0, 1, 0, -1, 1, 1, -1 };
+
+const int N      = (int) 2e5 + 5;
+const int mxN    = (int) 1e6 + 6;
+const int MOD    = (int) 1e9 + 7;
+const int INF    = (int) 1e9 + 9;
+const double EPS = (double) 1e-9;
+
+#define debug(x) cerr << #x << " = " << x << '\n';
+#define all(x)   x.begin(), x.end()
+#define szof(x)  (int) x.size()
+#define ff       first
+#define ss       second
+#define pb       push_back
+#define mp       make_pair
+#define PI       acos(0.0)
+#define TN       typename
+#define nl       '\n'
+#define Fast_IO  ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+inline int add(int a, int b, int mod) { a += b; return a >= mod ? a - mod : a < 0 ? a + mod : a; }
+inline int mult(int a, int b, int mod) { return (ll) a * b % mod; }
+
+template <TN T> inline void umin(T &x, T y) { x = x < y ? x : y; }
+template <TN T> inline void umax(T &x, T y) { x = x > y ? x : y; }
+
+template <TN T> T gcd(T a, T b) { return !b ? a : gcd(b, a % b); }
+template <TN T> T lcm(T a, T b) { return a * (b / gcd(a, b)); }
+
+int main() {
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+#endif
+    Fast_IO
+    int test = 1, tc = 0;
+    cin >> test;
+    while (test--) {
+        double r;
+        cin >> r;
+        ld sq = (4 * r * r);
+        ld circle = 2 * PI * r * r;
+        cout << "Case " << ++tc << ": " << fixed << setprecision(2) << (ld) (sq - circle) << nl;
+    }
+    return 0;
+}
