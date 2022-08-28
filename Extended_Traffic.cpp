38145b0382f93f4a084546c428d72e2c9c33f184
Md. Sajjat Hossen <111273800+Sajjat004@users.noreply.github.com>
2022-08-28 08:00:15 +0600

    Create Extended_Traffic.cpp

---
+++ Extended_Traffic.cpp
@@ -0,0 +1,119 @@
+/*---------------------------------------
+   BISMILLAHIR RAHMANIR RAHIM
+   AUTHOR : Md. Sajjat Hossen
+   TIME : 05-June,2021 10:32:41 PM
+----------------------------------------*/
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef  long long               ll;
+typedef  long double             ld;
+typedef  unsigned long long      ull;
+typedef  pair<int, int>          pii;
+typedef  pair<ll, ll>            pll;
+typedef  vector<int>             vi;
+typedef  vector<ll>              vll;
+typedef  tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds_set;
+typedef  tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> pbds_multiset;
+
+inline int Int() { int x; scanf("%d", &x); return x; }
+inline ll Long() { ll x; scanf("%lld", &x); return x; }
+
+int dx[8] = { 0, -1, 0, 1, -1, -1, 1, 1 };
+int dy[8] = { -1, 0, 1, 0, -1, 1, 1, -1 };
+
+const int N       = (int) 2e5 + 5;
+const int mxN     = (int) 1e6 + 6;
+const int MOD     = (int) 1e9 + 7;
+const ll INF      = (ll) 1e15 + 9;
+const double EPS  = (double) 1e-9;
+
+#define    debug(x)    cerr << #x << " = " << x << '\n';
+#define    all(x)      x.begin(), x.end()
+#define    szof(x)     (int) x.size()
+#define    ff          first
+#define    ss          second
+#define    pb          push_back
+#define    mp          make_pair
+#define    PI          acos(-1.0)
+#define    TN          typename
+#define    nl          '\n'
+#define    Fast_IO     ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+inline int add(int a, int b, int mod) { a += b; return a >= mod ? a - mod : a < 0 ? a + mod : a; }
+inline int mult(int a, int b, int mod) { return (ll) a * b % mod; }
+
+template <TN T> inline void umin(T &x, T y) { x = x < y ? x : y; }
+template <TN T> inline void umax(T &x, T y) { x = x > y ? x : y; }
+
+template <TN T> T gcd(T a, T b) { return !b ? a : gcd(b, a % b); }
+template <TN T> T lcm(T a, T b) { return a * (b / gcd(a, b)); }
+
+ll dis[202];
+
+struct edge {
+    int u, v;
+    ll w;
+    edge() {
+        u = v = 0;
+        w = 0LL;
+    }
+};
+
+int main() {
+    // Fast_IO
+    // clock_t tStart = clock();
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int test = Int(), tc = 0;
+    while (test--) {
+        int n = Int();
+        ll budyness[n + 2];
+        for (int i = 1; i <= n; ++i) {
+            budyness[i] = Long();
+            dis[i] = INF;
+        }
+        int m = Int();
+        edge e[m + 2];
+        for (int i = 1; i <= m; ++i) {
+            int s = Int(), t = Int();
+            ll x = budyness[t] - budyness[s];
+            x = (x * x * x);
+            e[i].u = s;
+            e[i].v = t;
+            e[i].w = x;
+        }
+
+        dis[1] = 0;
+
+        for (int i = 1; i < n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                if (dis[e[j].v] > dis[e[j].u] + e[j].w and dis[e[j].u] != INF) {
+                    dis[e[j].v] = dis[e[j].u] + e[j].w;
+                }
+            }
+        }
+
+        for (int j = 1; j <= m; ++j) {
+            if (dis[e[j].v] > dis[e[j].u] + e[j].w and dis[e[j].u] != INF) {
+                dis[e[j].v] = -INF;
+            }
+        }
+
+        int r = Int();
+        printf("Case %d:\n", ++tc);
+        for (int i = 1; i <= r; ++i) {
+            int d = Int();
+            ll x = dis[d];
+            if (x < 3 or x == INF) printf("?\n");
+            else printf("%lld\n", x);
+        }
+    }
+    // fprintf(stderr, "\nRuntime: %.10fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
+    return 0;
+}
